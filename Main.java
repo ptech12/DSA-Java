6998d0a297343721a76b004d9300adaf38138536
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-21 22:28:56 +0530

    DSA in Java starting

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println("Hey " + x);
+        scan.close();
+    }
+}
